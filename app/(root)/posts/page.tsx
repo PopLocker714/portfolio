b30330aff95186abffc49c3400c90881fa16e662
jojo <jojo@pop-os.localdomain>
2023-10-27 11:14:42 +0300

    save

--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -1,7 +1,4 @@
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -15,10 +12,17 @@ const Page = async () => {
     <main className="container max-w-[600px] pt-24 text-secondary">
       <h2 className="mb-6 text-3xl">Posts:</h2>
       {posts.map((post) => (
-        <Link  key={post.id} href={`/posts/${post.id}`}>
+        <Link key={post.id} href={`/posts/${post.id}`}>
           <Card className="mb-4">
             <CardContent className="flex p-0 items-center">
-              <Image className="mr-4" width={100} height={100} src={'https://via.placeholder.com/150/92c952'} alt="Post image"/><p className="hover:underline">{post.title}</p>
+              <Image
+                className="mr-4"
+                width={100}
+                height={100}
+                src={"https://via.placeholder.com/150/92c952"}
+                alt="Post image"
+              />
+              <p className="hover:underline">{post.title}</p>
             </CardContent>
           </Card>
         </Link>
